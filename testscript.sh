cc10ea46e7c146fc9926e2ccf45289b12f73a22e
f3r <f3r73ch@gmail.com>
2020-11-24 23:55:34 +0000

    changed testscript

--- testscript.sh
+++ testscript.sh
@@ -2,4 +2,4 @@
 echo "this script is from github f3asdadasdr"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This was added"
+echo "This was added after the 1st commit to the jenkins in another repo"
